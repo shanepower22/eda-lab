643738f2253914773f329d1760dbf503b7d77100
Shane Power <20102592@mail.wit.ie>
2025-11-25 13:58:35 +0000

    Filter topic subscribers.

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -89,6 +89,20 @@ export class EDAAppStack extends cdk.Stack {
     }
   );
 
+      const addMetadataFn = new lambdanode.NodejsFunction(
+      this,
+      "addMetadataFn",
+ {
+        runtime: lambda.Runtime.NODEJS_20_X,
+        entry: `${__dirname}/../lambdas/addImageMetadata.ts`,
+        timeout: cdk.Duration.seconds(15),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: imagesTable.tableName,
+        },
+    }
+ );
+
     // S3 --> SNS
     imagesBucket.addEventNotification(
         s3.EventType.OBJECT_CREATED,
@@ -102,6 +116,56 @@ export class EDAAppStack extends cdk.Stack {
     newImageTopic.addSubscription(
       new subs.SqsSubscription(mailerQ)
     );
+
+    newImageTopic.addSubscription(
+      new subs.LambdaSubscription(addMetadataFn, {
+        filterPolicy: {
+          metadata_type: sns.SubscriptionFilter.stringFilter({
+            allowlist: ["Caption", "Date", "Photographer"],
+      }),
+      },
+    })
+ );
+
+     newImageTopic.addSubscription(
+      new subs.SqsSubscription(imageProcessQueue, {
+        filterPolicyWithMessageBody: {
+          Records: sns.FilterOrPolicy.policy({
+            s3: sns.FilterOrPolicy.policy({
+              object: sns.FilterOrPolicy.policy({
+                key: sns.FilterOrPolicy.filter(
+                  sns.SubscriptionFilter.stringFilter({
+                    matchPrefixes: ["image"],
+                 })
+
+             ),
+           }),
+         }),
+         }),
+         },
+        rawMessageDelivery: true,
+      })
+ );
+
+     newImageTopic.addSubscription(
+      new subs.SqsSubscription(mailerQ, {
+        filterPolicyWithMessageBody: {
+          Records: sns.FilterOrPolicy.policy({
+            s3: sns.FilterOrPolicy.policy({
+              object: sns.FilterOrPolicy.policy({
+                key: sns.FilterOrPolicy.filter(
+                  sns.SubscriptionFilter.stringFilter({
+                    matchPrefixes: ["image"],
+                  })
+                ),
+              }),
+            }),
+           }),
+         },
+        rawMessageDelivery: true,
+      })
+ );
+
 
 
    // SQS --> Lambda
@@ -129,6 +193,8 @@ export class EDAAppStack extends cdk.Stack {
     // Permissions
 
     imagesTable.grantReadWriteData(processImageFn);
+    imagesTable.grantReadWriteData(addMetadataFn);
+
 
       mailerFn.addToRolePolicy(
       new iam.PolicyStatement({
@@ -149,5 +215,10 @@ export class EDAAppStack extends cdk.Stack {
     new cdk.CfnOutput(this, "bucketName", {
       value: imagesBucket.bucketName,
     });
+
+    new cdk.CfnOutput(this, "SNS Topic ARN", {
+      value: newImageTopic.topicArn ,
+     });
+
   }
 }
